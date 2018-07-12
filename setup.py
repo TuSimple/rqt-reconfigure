89c6398a3de76bafc8adecd75475110b1de5db86
diamondlx <515110597@qq.com>
2018-07-12 16:14:03 +0800

    remove useless code

--- setup.py
+++ setup.py
@@ -8,8 +8,6 @@ setup(
     version='0.4.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/rqt_resource',
                ["rqt_resource/editor_bool.ui", 
